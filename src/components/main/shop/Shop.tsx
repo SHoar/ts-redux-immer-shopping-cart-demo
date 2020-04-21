8a76586a8e460a4c878f5689c94ca80e841c6ae8
Sean Hoar <sean.hoar@gmail.com>
2020-04-21 10:39:50 -0400

    works in codesandbox

--- src/components/main/shop/Shop.tsx
+++ src/components/main/shop/Shop.tsx
@@ -1,43 +1,52 @@
 import React, { useEffect } from "react";
 // import { Container, Header, Card } from "semantic-ui-react"
-import { Container, CardHeader, GridList, GridListTile} from "@material-ui/core"
-import { RootState } from '../../../redux/index';
+import {
+  Container,
+  CardHeader,
+  GridList,
+  GridListTile
+} from "@material-ui/core";
+import { RootState } from "../../../redux/index";
 import { bindActionCreators, Dispatch } from "redux";
-import { loadProducts } from '../../../redux/modules/products';
+import { loadProducts } from "../../../redux/modules/products";
 import { connect } from "react-redux";
 import { ProductCard } from "./ProductCard";
 
 const mapStateToProps = (state: RootState) => ({
-    products: state.products.products
+  products: state.products.products
 });
 
-const mapDispatchToProps = (dispatch: Dispatch) => 
-    bindActionCreators(
-        {
-            loadProducts
-        },
-        dispatch
-    );
+const mapDispatchToProps = (dispatch: Dispatch) =>
+  bindActionCreators(
+    {
+      loadProducts
+    },
+    dispatch
+  );
 
-type Props = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
+type Props = ReturnType<typeof mapStateToProps> &
+  ReturnType<typeof mapDispatchToProps>;
 
 const UnconnectedShop: React.FC<Props> = ({ loadProducts, products }) => {
-    useEffect(() => {
-        if(products.length === 0) loadProducts();
-    }, [loadProducts, products]);
+  useEffect(() => {
+    if (products.length === 0) loadProducts();
+  }, [loadProducts, products]);
 
-    return (
-        <Container>
-            <CardHeader variant="h2">Shop</CardHeader>
-            <GridList cellHeight={160} cols={3}>
-                {products.map(product => (
-                    <GridListTile key={product.id}>
-                        <ProductCard product={product} />
-                    </GridListTile>
-                ))}
-            </GridList>
-        </Container>
-    )
+  return (
+    <Container>
+      <CardHeader variant="h2">Shop</CardHeader>
+      <GridList cellHeight={350} cols={3}>
+        {products.map(product => (
+          <GridListTile key={product.id}>
+            <ProductCard product={product} />
+          </GridListTile>
+        ))}
+      </GridList>
+    </Container>
+  );
 };
 
-export const Shop = connect(mapStateToProps, mapDispatchToProps)(UnconnectedShop);
+export const Shop = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(UnconnectedShop);
--- src/redux/modules/products.ts
+++ src/redux/modules/products.ts
@@ -1,86 +1,90 @@
 // TODO: We should move typedAction elsewhere since it's shared
-import { typedAction } from './users';
-import { Dispatch, AnyAction } from 'redux';
-import { sampleProducts } from '../../sampleData/sampleProducts';
+import { typedAction } from "./users";
+import { Dispatch, AnyAction } from "redux";
+import { sampleProducts } from "../../sampleData/sampleProducts";
 import { produce as p } from "immer";
 
 export type Product = {
-    id: number;
-    name: string;
-    price: number;
-    img: string;
-}
+  id: number;
+  name: string;
+  price: number;
+  img: string;
+};
 
 export type CartItem = {
-    id: number;
-    quantity: number;
-}
+  id: number;
+  quantity: number;
+};
 
 type ProductState = {
-    products: Product[],
-    loading: boolean,
-    cart: CartItem[]
-}
+  products: Product[];
+  loading: boolean;
+  cart: CartItem[];
+};
 
 const initialState: ProductState = {
-    products: [],
-    loading: false,
-    cart: [],
+  products: [],
+  loading: false,
+  cart: []
 };
 
 export const addProducts = (products: Product[]) => {
-    return typedAction('products/ADD_PRODUCTS', products);
+  return typedAction("products/ADD_PRODUCTS", products);
 };
 
 export const addToCart = (product: Product, quantity: number) => {
-    return typedAction('products/ADD_TO_CART', { product, quantity})
+  return typedAction("products/ADD_TO_CART", { product, quantity });
+};
+
+export const getCart = () => {
+  return typedAction("products/GET_CART");
 };
 
 // Action creator returning a thunk!
 export const loadProducts = () => {
-    return (dispatch: Dispatch<AnyAction>) => {
-        setTimeout(() => {
-            // pretend to load an item
-            dispatch( addProducts(sampleProducts))
-        }, 500);
-    }
-}
+  return (dispatch: Dispatch<AnyAction>) => {
+    setTimeout(() => {
+      // pretend to load an item
+      dispatch(addProducts(sampleProducts));
+    }, 500);
+  };
+};
 
 type ProductAction = ReturnType<typeof addProducts | typeof addToCart>;
 
 export function productsReducer(
-    state = initialState,
-    action: ProductAction
+  state = initialState,
+  action: ProductAction
 ): ProductState {
-    switch(action.type) {
-        case 'products/ADD_PRODUCTS':
-            return {
-                ...state,
-                products: [...state.products, ...action.payload],
-            };
+  switch (action.type) {
+    case "products/ADD_PRODUCTS":
+      return {
+        ...state,
+        products: [...state.products, ...action.payload]
+      };
 
-            // return p<ProductState>(state, (draftState) => {
-            //     draftState.products.push(...action.payload);
-            // });
-        case 'products/ADD_TO_CART':
-            return {
-                ...state,
-                cart: [
-                    ...state.cart,
-                    {
-                        id: action.payload.product.id,
-                        quantity: action.payload.quantity
-                    }
-                ]
-            };
-            // return p<ProductState>(state, draftState => {
-            //     draftState.cart.push({ 
-            //         id: action.payload.product.id,
-            //         quantity: action.payload.quantity 
-            //     })
-            // });
-            
-        default:
-            return state;
-    }
+    // return p<ProductState>(state, (draftState) => {
+    //     draftState.products.push(...action.payload);
+    // });
+    case "products/ADD_TO_CART":
+      return {
+        ...state,
+        cart: [
+          ...state.cart,
+          {
+            id: action.payload.product.id,
+            quantity: action.payload.quantity
+          }
+        ]
+      };
+    // return p<ProductState>(state, draftState => {
+    //     draftState.cart.push({
+    //         id: action.payload.product.id,
+    //         quantity: action.payload.quantity
+    //     })
+    // });
+
+    default:
+      return state;
+  }
 }
